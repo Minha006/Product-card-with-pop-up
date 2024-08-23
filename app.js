d948e3da4d64b77416b43cfbcbec4a767c4c1526
Nageen Yousuf <125158297+Minha006@users.noreply.github.com>
2024-08-23 13:49:21 -0700

    Created an interactive product card featuring a pop-up display for product details. Leveraging HTML, CSS, and JavaScript, this project demonstrates my ability to build responsive and engaging UI components with a focus on usability and visual appeal.
     A dynamic product card with a pop-up feature designed using HTML, CSS, and JavaScript. This project showcases a modern and interactive way to display product details, enhancing user engagement with a smooth, visually appealing pop-up effect.

---
+++ app.js
@@ -0,0 +1,34 @@
+const modal =  document.querySelectorAll('.modal'),
+     cardBtn = document.querySelectorAll('.card--product'),
+     modalClose = document.querySelectorAll('.modal--close'),
+     modalCard = document.querySelectorAll('.modal--card')
+
+
+     let activeModal = (modalClick) =>  {
+        modal[modalClick].classList.add('active-modal');
+     }
+
+     cardBtn.forEach((cardBtn, i) => {
+        cardBtn.addEventListener('click', () =>{
+            activeModal(i)
+        })
+     } )
+modalClose.forEach((modalClose) =>{
+   modalClose.addEventListener('click', () =>{
+    modal.forEach((modalRemove) =>{
+        modalRemove.classList.remove('active-modal')
+    })
+   })
+})
+
+modal.forEach((modal) =>{
+    modal.addEventListener('click',() =>{
+        modal.classList.remove('active-modal')
+    })
+})
+
+modalCard.forEach((modalCard) =>{
+    modalCard.addEventListener('click',(e) =>{
+        e.stopPropagation()
+    })
+})
